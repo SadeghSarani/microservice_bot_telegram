a44fecb8e4cee9abce27f7bb2360e81f8e48426f
azimi <m.azimi@limoo.host>
2025-02-23 21:04:45 +0330

    add calback

--- app/Http/Controllers/PayController.php
+++ app/Http/Controllers/PayController.php
@@ -2,21 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserPay;
 use Illuminate\Http\Request;
-use Zarinpal\Zarinpal;
+use Illuminate\Support\Facades\Http;
 
 class PayController extends Controller
 {
-    public function calback(Request $request,Zarinpal $zarinpal){
+    public function calback(Request $request){
 
-        $payment = [
-            'authority' => $request->input('Authority'), 
-            'amount'    => 5000
+
+        $authority = $request->query('Authority');
+
+        $userPay = UserPay::where('authority', $authority)
+        ->where('status', 'pending')
+        ->first();
+
+        if (!$userPay) {
+            return 'پرداخت نا موفق';
+        }
+        
+
+
+        $data = [
+            "merchant_id" => env('ZARINPAL_MERCHANTID'),
+            "authority" => $_GET['Authority'],
+            "amount" => $userPay['price']
         ];
-        $response = $zarinpal->verify($payment);
+        $response = Http::withHeaders([
+            'User-Agent' => 'ZarinPal Rest Api v4',
+            'Content-Type' => 'application/json'
+        ])->post('https://sandbox.zarinpal.com/pg/v4/payment/verify.json', $data);
+    
+        $result = $response->json();
+    
+        
+    
+        if (isset($result['data']['code']) && $result['data']['code'] == 100) {
+
+            
+            $userPay->update([
+                'status' => 'active',
+            ]);
+            return view('calback');
+
+        } else {
+            return 'نا موفق';
+        }
 
 
 
-        return view('calback');
     }
 }
--- app/Models/UserPay.php
+++ app/Models/UserPay.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserPay extends Model
 {
-    //
+    protected $guarded = [];
+    
 }
