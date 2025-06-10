af427bda9625e8661fa7f08eb21ad0f223b6a7ca
sadegh sarani <banibk549@gmail.com>
2025-06-10 19:43:06 +0330

    add messages

--- app/Jobs/AiJobDietMessage.php
+++ app/Jobs/AiJobDietMessage.php
@@ -37,12 +37,14 @@ class AiJobDietMessage implements ShouldQueue
     public function handle(): void
     {
         try {
-            $htmlContent = Ai::sendMessage($this->data['chat'], $this->data['prompt'], $this->data['chat_id']);
+            $htmlContent = Ai::sendMessage($this->data['chat'], '', $this->data['chat_id']);
             if ($htmlContent) {
                 $this->telegramBot->send(
                     $this->data['user_telegram_id'],
                     "https://calorieno.com/diet/user/".$this->data['chat_id'].  "
-                    کاربر گرامی رژیم شما آماده شده است و از طریق لینک زیر اقدام نمایید نسبت به دیدن و ذخیره لطفا لینک را کپی کرده و در مرورگر خود بازنمایید تا بتوانید آن را دانلود نمایید",
+                    💚 #رژیم غذایی ۷ روزه کالری نو!
+🥝 از طریق لینک بالا می‌تونی رژیم ات رو‌ مشاهده کنی و در صورتی که نیاز به دریافت PDF داری، کافیه لینک را در مرورگر خودت کپی کنی و از گزینه دریافت PDF استفاده کنی.
+از 'کالری' کوچ یادت نره! 😍 تو یک متخصص تغذیه اختصاصی داری که در هر لحظه از شبانه روز کنارت هست و قرار هست در گرفتن رژیم کمکت کنه! 👌 کافیه هر زمان سوال داری از  منو گزینه 'کالری' کوچ رو انتخاب کنی!",
                 );
             }
         } catch (\Exception $exception) {
